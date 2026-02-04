8cd83bf88ae580a134d85257211d0f2b8fe05536
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2026-02-04 17:21:19 +0530

    product-service, create product and get all products endpoints done

---
+++ product-service/src/main/java/com/programmingtechie/product_service/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.programmingtechie.product_service.service;
+
+import com.programmingtechie.product_service.dto.ProductRequest;
+import com.programmingtechie.product_service.dto.ProductResponse;
+import com.programmingtechie.product_service.model.Product;
+import com.programmingtechie.product_service.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    // create product
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product); // saved product data in database
+        log.info("Product {} is saved", product.getId());   // print log in console
+    }
+
+    // get all products
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductResponse> productResponses = products.stream()
+                .map(product -> mapToProductResponse(product)) // change each Product object to ProductResponse object using map() from stream
+                .collect(Collectors.toList());
+        return productResponses;
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
